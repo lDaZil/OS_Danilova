6e5e0a5c24ff0f651dfa87466998a8f9f3cf4348
Zinaida Danilova <70945233+lDaZil@users.noreply.github.com>
2021-04-29 10:20:06 +0700

    Update lab5.c

--- lab5.c
+++ lab5.c
@@ -4,63 +4,107 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
-const long LINES = 429496729;
+#define N 100
 
 int main(int argc, char *argv[]) {
-    long fileOffsets[300];
-    int fd = 0, i = 1, j = 0, lineNumber = 0, lineLength[300] = {-1};
-    char c = 0, *buf;
-    if((buf = (char*) malloc(257 * sizeof(char))) == NULL) {
-        exit(1);
-    }
-    if (argc < 2) {
-        perror("Usage: filename as argument");
-        exit(2);
-    }
-    if((fd = open(argv[1], O_RDONLY)) == -1) {
-        perror(strcat("Input file doesn't exist - ", argv[1]));
-        exit(1);
-    }
-    fileOffsets[0] = 0L;
-    fileOffsets[1] = 0L;
-       while(read(fd, &c, 1)) {
-        if(i == LINES - 1) {
-            perror("File is large");
-        }
-        if(c == '\n') {
-            j++;
-            lineLength[i++] = j;
-            fileOffsets[i] = lseek(fd, 0L, SEEK_CUR);
-            j = 0;
-        }
-        else {
-            j++;
-        }
-    }
-    int bufSize = 257;
-    while(printf("Enter line number : ") && scanf("%d", &lineNumber)) {
-        if(lineNumber == 0) {
-            exit(0);
-        }
-        if(lineNumber < 0 || lineNumber > LINES || (lineLength[lineNumber] == -1)) {
-            fprintf(stderr, "Wrong line number!!!\n");
-            continue;
-        }
-        lseek(fd, fileOffsets[lineNumber], SEEK_SET);
-        if (lineLength[lineNumber] > bufSize) {
-            if(realloc(buf, lineLength[lineNumber] * sizeof(char))) {
-                exit(1);
-            }
-            bufSize = lineLength[lineNumber];
-        }
-        if(read(fd, buf, lineLength[lineNumber])) {
-            write(1, buf, lineLength[lineNumber]);
-        }
-        else {
-            fprintf(stderr, "Wrong line number!!!\n");
-        }
-    }
-    free(buf);
-    return 0;
+	long fileOffsets[N] = {0};
+	int fileDescriptor;
+	int lineLength[N] = {-1};
+	int bufSize = 32;
+	char *buf = (char*)malloc(sizeof(char) * bufSize);
+
+	if (buf == NULL) {
+		perror("Error memory allocation ");
+		exit(1);
+	}
+
+	if ((fileDescriptor = open("test.txt", O_RDONLY)) == -1) {
+		perror("File doesn't open");
+		free(buf);
+		exit(1);
+	}
+
+	int i = 1, j = 0;
+	int r = 1;
+	int oldFileOffsets = 0;
+	while (r) {
+		r = read(fileDescriptor, buf, bufSize);
+		if (r == -1) {
+			if (errno == EAGAIN || errno == EINTR) {
+				r = 1;
+			} else {
+				r = 0;
+				fprintf(stderr, "Error read occurred\n");
+			}
+		}
+		for (int k = 0; k < r; ++k) {
+			++j;
+			if (buf[k] == '\n') {
+				lineLength[i] = j;
+				fileOffsets[++i] = j + oldFileOffsets;
+				oldFileOffsets += j;
+				j = 0;
+			}
+		}
+	}
+
+	int lineNumber = 0;
+	printf("Enter line number: ");
+	while (scanf("%d", &lineNumber)) {
+		if (!lineNumber) {
+			free(buf);
+			if (close (fileDescriptor) != 0) {
+				fprintf (stderr, "Cannot close file (descriptor=%d)\n", fileDescriptor);
+				exit (1);
+			}
+			exit (0);
+		}
+		if (lineNumber < 0 || lineNumber > (N - 1) || (fileOffsets[lineNumber + 1] == 0)) {
+			fprintf(stderr, "wrong line number \n");
+			printf("Enter line number: ");
+			continue;
+		}
+		lseek(fileDescriptor, fileOffsets[lineNumber], SEEK_SET);
+		if (lineLength[lineNumber] > bufSize) {
+			if (realloc (buf, lineLength[lineNumber] * sizeof(char)) == NULL) {
+				perror("Error memory allocation");
+				free (buf);
+				if (close (fileDescriptor) != 0) {
+					fprintf (stderr, "Cannot close file (descriptor=%d)\n", fileDescriptor);
+				}
+				exit(1);
+			}
+			bufSize = lineLength[lineNumber];
+		}
+
+		r = 1;
+		int w = -1;
+		while (r) {
+			r = read(fileDescriptor, buf, lineLength[lineNumber]);
+			if (r == -1) {
+				if (errno == EAGAIN || errno == EINTR) {
+					r = 1;
+				} else {
+					r = 0;
+					fprintf(stderr, "Error read occurred\n");
+				}
+			}
+			while (w != lineLength[lineNumber]) {
+				w = write (STDOUT_FILENO, buf, lineLength[lineNumber]);
+				if (w == -1) {
+					if (errno == EAGAIN || errno == EINTR) {
+						continue;
+					} else {
+						w = lineLength[lineNumber];
+						fprintf(stderr, "Error write occurred\n");
+					}
+				}
+			}
+		}
+		printf("Enter line number: ");
+
+	}
+	return 0;
 }
